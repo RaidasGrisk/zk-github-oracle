03a0a422b19da7857968dc52c6c8e517b7a9cbfc
raidas <mrraidas@gmail.com>
2023-05-25 12:38:42 +0300

    use fetch instead of axios, up snarkyjs version

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const { isReady, PrivateKey, Field, Signature, CircuitString } = require("snarkyjs")
+const { isReady, PrivateKey, Field, Signature } = require("snarkyjs")
 const express = require('express')
 const bodyParser = require('body-parser')
-const axios = require('axios')
 const cors = require('cors')
 require('dotenv').config()
 
@@ -13,7 +12,6 @@ const PORT = 8080
 
 // mina config
 const PRIVATE_KEY = process.env.PRIVATE_KEY
-console.log(PRIVATE_KEY)
 
 const checkGithubUser = async (personal_access_token) => {
 
@@ -24,16 +22,14 @@ const checkGithubUser = async (personal_access_token) => {
     'Authorization': `Bearer ${personal_access_token}`,
     'X-GitHub-Api-Version': '2022-11-28'
   }
-  let response = await axios.request({
-    method: 'get',
-    url: url,
+  let response = await fetch(url, {
+    method: 'GET',
     headers: headers_,
   }).catch(err => {
     // this is not nice at all but will do for now
-    return { data: {} }
+    return { id: undefined }
   })
-
-  return response
+  return await response.json()
 }
 
 app.post('/auth', async (req, res) => {
@@ -44,13 +40,11 @@ app.post('/auth', async (req, res) => {
   // VALID   - { data: { id: 10008368,, ... }
   const personal_access_token = req.body.personal_access_token
   const response = await checkGithubUser(personal_access_token)
-  const isValidUser = response.data.id ? true : false
+  const isValidUser = response.id ? true : false
 
   // now the snarkyjs magick
   await isReady
   const isValidUser_ = Field(isValidUser)
-  // TODO: lets rethink this, does signature with data
-  // as simple as this is enough for security..?
   const privateKey = PrivateKey.fromBase58(PRIVATE_KEY)
   const publicKey = privateKey.toPublicKey()
   const signature = Signature.create(privateKey, [isValidUser_])
